b6c1f93c9f737ec94d896b950dd92efe0c4306e9
Romain Francois <romain@thinkr.fr>
2017-10-18 09:33:22 +0200

    :baby:

---
+++ R/html_document2.R
@@ -0,0 +1,26 @@
+#' almost same as [rmarkdown::html_document()]
+#'
+#' @importFrom readr read_file write_lines
+#' @importFrom rmarkdown html_document
+#' @importFrom stringr str_replace_all
+#' @export
+html_document2 <- function(...){
+  format <- html_document(...)
+
+  pre_knit <- format$pre_knit
+  format$pre_knit <- function(...){
+    frames <- sys.frames()
+    e <- frames[[ length(frames) - 1 ]]
+
+    old_input <- e$knit_input
+    new_input <- tempfile( fileext = "Rmd" )
+    e$knit_input <- new_input
+
+    lines <- read_file(old_input) %>%
+      str_replace_all("(````.*?\n)```", "\\1`r ''````"  )
+    write_lines( lines , new_input )
+
+    pre_knit(...)
+  }
+  format
+}
